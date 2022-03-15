bacf82891af24e7158132b5fae50a4731b7b0166
Maurice <maurice.baez1@gmail.com>
2022-03-15 16:28:35 -0700

    finished ajax request fxn

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Let's get this party started!");
+"use strict"
+
+/*Create an async function creating an api request to the giphy server
+
+grab the data thats returned, input into another function to append to the DOM
+
+the info we'll grab from the server is the url of the image
+
+We'll take the image and pass that to another function to append it to the DOM.
+
+We need to add event listeners to our search and remove buttons.
+
+We'll need a function to remove all images.*/
+
+
+async function getGif() {
+  let searchTerm = $("#gif").val();
+  let key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+
+  let response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q: searchTerm, api_key:key }});
+
+  console.log("resp=", response);
+  
+}
+
+const $searchButton = $("#search");
+const $deleteButton = $("#remove");
+
+$searchButton
+.on("click", getGif);
+
+
+
