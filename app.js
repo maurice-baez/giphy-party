23b815032fd8a811d8f8c1eb19f54e3886e89d98
Michael Paglione <pagman777@gmail.com>
2022-03-15 20:08:04 -0400

    functional data pull

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 /*Create an async function creating an api request to the giphy server
 
@@ -12,22 +12,39 @@ We need to add event listeners to our search and remove buttons.
 
 We'll need a function to remove all images.*/
 
+/** Add gif by appending server result to DOM */
+function addGif(url) {
+  console.log("url =", url);
 
+  let $gifArea = $(".display");
+  let $img = $("<img>");
+
+  $img.attr("src", url);
+
+  $gifArea.append($img);
+}
+
+
+/**Gather information from search */
 async function getGif() {
   let searchTerm = $("#gif").val();
-  let key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+  let key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 
-  let response = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q: searchTerm, api_key:key }});
+  let response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: { q: searchTerm, api_key: key } });
 
   console.log("resp=", response);
-  
+  //Get random number between 0-50
+  let randomNumber = randomNumber()
+  addGif(response.data.data[randomNumber].images.original.url);
 }
 
 const $searchButton = $("#search");
 const $deleteButton = $("#remove");
 
 $searchButton
-.on("click", getGif);
+  .on("click", getGif);
 
-
-
+/**Get random number between 0-50 */
+function randomNumber(){
+  return Math.floor(Math.random() * 50)
+}
